c7a7cafa028bb7474575ea0573924b8b3ae1c274
wobsoriano <sorianorobertc@gmail.com>
2022-11-22 09:45:54 -0800

    add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Markdoc from '@markdoc/markdoc'
+import render from 'vue-markdoc'
+import Callout from './Callout.vue'
+
+const tags = {
+  callout: {
+    render: Callout,
+    attributes: {},
+  },
+}
+
+const doc = `
+{% callout %}
+Attention, over here!
+{% /callout %}
+`
+
+const ast = Markdoc.parse(doc)
+const content = Markdoc.transform(ast, { tags })
+
+const ContentComponent = render(content)
+</script>
+
+<template>
+  <ContentComponent />
+</template>
---
+++ playground/src/Callout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="callout">
+    <slot />
+  </div>
+</template>
+
+<style>
+.callout {
+  height: 50px;
+  width: 50px;
+  background: Red;
+}
+</style>
