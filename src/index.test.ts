1b2b51ca23e972691d6c2b94920831a32b60e392
wobsoriano <sorianorobertc@gmail.com>
2022-11-22 09:46:51 -0800

    update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,7 @@
+/**
+ * @vitest-environment jsdom
+ */
+
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import type { RenderableTreeNode, RenderableTreeNodes } from '@markdoc/markdoc'
